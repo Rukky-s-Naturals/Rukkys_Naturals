78cfc4dbcce80081926b5db356385b90b305f6ae
Dzidm <mpyadzidzo@yahoo.com>
2025-05-23 11:06:21 +0000

    effect changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router';
 import {FaInstagram, FaLinkedinIn, FaFacebook, FaTiktok, FaMapMarkerAlt, FaEnvelope,FaPhoneAlt} from 'react-icons/fa';
 
 const Footer = () => {
